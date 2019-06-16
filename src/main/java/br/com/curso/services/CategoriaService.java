ff868754e0c1151d34da2bc24fe295a315ea4534
Luciano Nascimento <luciano_lpn22@yahoo.com.br>
2019-06-16 13:42:32 -0300

    criando repository e service para Categoria

---
+++ src/main/java/br/com/curso/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package br.com.curso.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.curso.domain.Categoria;
+import br.com.curso.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	@Autowired
+	CategoriaRepository repo;
+
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> categoria= repo.findById(id);
+		return categoria.orElse(null);
+	}
+}
