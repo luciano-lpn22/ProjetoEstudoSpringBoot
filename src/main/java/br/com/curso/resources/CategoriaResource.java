a95c3bde0613509f3840a419b1596f4ca20607b2
Luciano Nascimento <luciano_lpn22@yahoo.com.br>
2019-06-16 12:34:57 -0300

    Testando classe Categoria no Rest

--- src/main/java/br/com/curso/resources/CategoriaResource.java
+++ src/main/java/br/com/curso/resources/CategoriaResource.java
@@ -1,16 +1,27 @@
 package br.com.curso.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.curso.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "rest OK";
+	public List<Categoria>listar() {
+		
+		Categoria c1= new Categoria(1, "Informatica");
+		Categoria c2= new Categoria(2, "Escritorio");
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(c1);
+		lista.add(c2);
+		return lista;
 	}
 	
 }
