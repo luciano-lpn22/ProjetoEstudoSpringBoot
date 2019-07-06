905dabed071463f692ac06f0af28fee97666ef37
Luciano Nascimento <luciano_lpn22@yahoo.com.br>
2019-07-06 11:30:19 -0300

    testando envio produção

--- src/main/java/br/com/compra/config/ProdConfig.java
+++ src/main/java/br/com/compra/config/ProdConfig.java
@@ -2,36 +2,17 @@ package br.com.compra.config;
 
 import java.text.ParseException;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-
-import br.com.compra.services.DBService;
 
 @Configuration
 @Profile("prod")
 public class ProdConfig {
 
-	@Autowired
-	private DBService service;
-	
-
-	@Value("${spring.jpa.hibernate.ddl-auto}")
-	private String strategy;
 		
 	@Bean
 	public boolean instatiateDatabase() throws ParseException {
-		if (!strategy.equals("create")) {
-			return false;
-		}
-		try {
-			service.instantiateTestDataBase();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		return true;
 	
 	}
