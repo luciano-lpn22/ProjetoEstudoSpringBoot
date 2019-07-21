5d44c7005df0621728767a2f21ef02afe2f2077c
Luciano Nascimento <luciano_lpn22@yahoo.com.br>
2019-07-21 14:25:31 -0300

    Salvando Imagem no Amazon S3

--- src/main/java/br/com/compra/CompramcApplication.java
+++ src/main/java/br/com/compra/CompramcApplication.java
@@ -1,16 +1,30 @@
 package br.com.compra;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.compra.services.S3Service;
+
 @SpringBootApplication
 public class CompramcApplication implements  CommandLineRunner{
 
+	@Autowired
+	private S3Service service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CompramcApplication.class, args);
 	}
 	@Override
-	public void run(String... args) throws Exception {}
+	public void run(String... args) throws Exception {
+		
+		String localFilePath;
+		localFilePath="C:\\Users\\Public\\Pictures\\Sample Pictures\\Koala.jpg";
+		
+		
+		service.uploadFile(localFilePath);
+		
+	}
 
 }
